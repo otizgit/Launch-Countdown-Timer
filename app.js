0291e974589b2b402d98c24ec7eeca972b064532
Emmanuel <emmanueljoe777@gmail.com>
2023-02-14 04:08:10 +0100

    new Date

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countDown = () => {
-  const countDate = new Date("January 10, 2023 00:00:00").getTime();
+  const countDate = new Date("December 11, 2023 00:00:00").getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
